03685c65ec8b7d53cee5dd46708664fa2944c90f
Buga <120481193+BugaRodrigues@users.noreply.github.com>
2024-09-14 21:20:06 -0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -4,19 +4,25 @@ using namespace std;
 int main() {
 	int primeiro = 0;
 	int segundo = 1;
-	int terceiro = 0;
-	int termo = 0;
+	int terceiro = primeiro + segundo;
+	int numero = 0;
 
 	cout << "quantos termos voce quer ver? \n";
-	cin >> termo;
+	cin >> numero;
 
-	while (termo > 0) {
-		cout << primeiro + segundo << ", ";
-		segundo += primeiro;
+	while (true) {
+		if (numero == terceiro) {
+			cout << "Pertence a sequencia. \n";
+			break;
+		} else if (terceiro > numero) {
+			cout << "Nao pertence a sequencia. \n";
+			break;
+		}
+		terceiro = primeiro + segundo;
 		primeiro = segundo;
-		termo--;
+		segundo = terceiro;
 	}
-
+	//hellow world
 	system("pause");
 
 	return 0;
